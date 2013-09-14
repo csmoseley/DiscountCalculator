b51efa2f887b8138f778f6f2e4d4a0067d46dc50
Christopher Moseley <cs@csmoseley.com>
2013-09-13 22:58:13 -0400

    Assignment Finished
    Added programming logic, added error handling. Should work fine now.

--- gen/com/csmoseley/discountcalculator/R.java
+++ gen/com/csmoseley/discountcalculator/R.java
@@ -19,13 +19,19 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int fontSizePercentsDiscount=0x7f040002;
+        public static final int linePadding=0x7f040003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int editText1=0x7f080002;
+        public static final int action_settings=0x7f080011;
+        public static final int customValueText=0x7f08000b;
+        public static final int dynPayText=0x7f08000f;
+        public static final int dynSaveText=0x7f08000d;
+        public static final int exitButton=0x7f080010;
+        public static final int fieldEntry=0x7f080002;
         public static final int linlayout=0x7f080000;
         public static final int radio0=0x7f080005;
         public static final int radio1=0x7f080006;
@@ -33,6 +39,8 @@ public final class R {
         public static final int radio3=0x7f080008;
         public static final int radioGroup1=0x7f080004;
         public static final int seekBar1=0x7f08000a;
+        public static final int textPay=0x7f08000e;
+        public static final int textSaved=0x7f08000c;
         public static final int textView1=0x7f080003;
         public static final int textView2=0x7f080001;
         public static final int textView3=0x7f080009;
@@ -51,7 +59,14 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int customText=0x7f050007;
+        public static final int customValue=0x7f050008;
+        public static final int dynPay=0x7f05000b;
+        public static final int dynSaved=0x7f05000c;
+        public static final int editTextHint=0x7f05000e;
+        public static final int exit=0x7f05000d;
         public static final int listedprice=0x7f050002;
+        public static final int youPay=0x7f05000a;
+        public static final int youSaved=0x7f050009;
     }
     public static final class style {
         /** 
--- src/com/csmoseley/discountcalculator/MainActivity.java
+++ src/com/csmoseley/discountcalculator/MainActivity.java
@@ -1,23 +1,134 @@
 package com.csmoseley.discountcalculator;
 
-import android.os.Bundle;
+import java.text.NumberFormat;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		EditText input = (EditText) findViewById(R.id.fieldEntry);
+		input.addTextChangedListener(typingListener);
+		SeekBar seek = (SeekBar) findViewById(R.id.seekBar1);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
+			@Override
+			public void onProgressChanged(SeekBar arg0, int value, boolean arg2) {
+				// change custom value and update if custom selected.
+				RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
+				int selected = radioGroup.getCheckedRadioButtonId();
+				TextView customValText = (TextView) findViewById(R.id.customValueText);
+				customValText.setText(Integer.toString(value) + "%");
+				if ((selected - 2131230725) == 3) {
+					updateValue(value);
+				}
+			}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
+	}
+
+	public void updateValue(View view) {
+		updateValue();
+	}
+
+	public void updateValue() {
+		SeekBar customVal = (SeekBar) findViewById(R.id.seekBar1);
+		Double doubleCustom = (double) customVal.getProgress();
+		updateValue(doubleCustom);
+	}
+
+	public void updateValue(double custom) {
+		TextView saved = (TextView) findViewById(R.id.dynSaveText);
+		TextView pay = (TextView) findViewById(R.id.dynPayText);
+		EditText edit = (EditText) findViewById(R.id.fieldEntry);
+		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
+		double total;
+		if (0 != edit.getText().toString().length()) {
+			total = Double.parseDouble(edit.getText().toString());
+		} else {
+			total = 0.00;
+			edit.setError("Enter List Price");
+		}
+		double percent = custom;
+		if (total == 0) {
+			saved.setText("$0.00");
+			pay.setText("$0.00");
+		} else {
+			int selected = radioGroup.getCheckedRadioButtonId();
+			Log.i("percent", Integer.toString(selected));
+			switch (selected - 2131230725) {
+			case 0:
+				percent = .1;
+				break;
+			case 1:
+				percent = .25;
+				break;
+			case 2:
+				percent = .50;
+				break;
+			case 3:
+				percent = custom / 100;
+				break;
+			}
+			NumberFormat nf = NumberFormat.getCurrencyInstance();
+			saved.setText(nf.format(percent * total));
+			pay.setText(nf.format((1 - percent) * total));
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private TextWatcher typingListener = new TextWatcher() {
+		@Override
+		public void afterTextChanged(Editable arg0) {
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+				int arg3) {
+
+		}
+
+		@Override
+		public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+				int arg3) {
+			// change values if edited.
+			updateValue();
+		}
+
+	};
+
+	public void exitApp(View v) {
+		finish();
+	}
 }
